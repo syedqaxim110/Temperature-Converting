75462dd1ac7384c2f73a7ef48bed3bb6ad536577
Syed Qasim <87519159+syedqaxim110@users.noreply.github.com>
2021-09-23 15:27:43 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperature_converting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the amount of celsius: ");
+            int celsius = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Kelvin = {0}", celsius + 273);
+            Console.WriteLine("Fahrenheit = {0}", celsius * 18 / 10 + 32);
+            int Kelvin = Convert.ToInt32(Console.ReadLine());
+            int Fahrenheit = Convert.ToInt32(Console.ReadLine());
+    
+        }
+    }
+}
